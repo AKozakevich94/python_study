3dc00ea9f68132fb8350a49b4a734ab69bb4c683
Alex <kozakevich.lanjeron@gmail.com>
2022-09-18 19:20:07 +0300

    pyGame Space invaders L1-3

---
+++ Study/PyGame/bullet.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, screen, gun):
+        """new bullet in gun position"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+        self.rect = pygame.Rect(0, 0, 2, 12)
+        self.color = 30, 230, 86
+        self.speed = 1.5
+        self.rect.centerx = gun.rect.centerx
+        self.rect.top = gun.rect.top
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """move bullet up"""
+        self.y -= self.speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """draw bullet on screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
---
+++ Study/PyGame/controls.py
@@ -0,0 +1,41 @@
+import pygame, sys
+from bullet import Bullet
+
+
+def events(screen, gun, bullets):
+    """event handling"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            """button right"""
+            if event.key == pygame.K_RIGHT:
+                gun.mright = True
+            elif event.key == pygame.K_LEFT:
+                gun.mleft = True
+            elif event.key == pygame.K_SPACE:
+                new_bullet = Bullet(screen, gun)
+                bullets.add(new_bullet)
+        elif event.type == pygame.KEYUP:
+            """button right"""
+            if event.key == pygame.K_RIGHT:
+                gun.mright = False
+            elif event.key == pygame.K_LEFT:
+                gun.mleft = False
+
+
+def update(bg_color, screen, gun, bullets):
+    """update screen"""
+    screen.fill(bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    gun.output()
+    pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """update bullet position"""
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
