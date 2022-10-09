64142233afbe14a0ec46bfd3eb55aa640bd04f75
Alex <kozakevich.lanjeron@gmail.com>
2022-10-09 16:35:21 +0300

    Aiogram pizza delivery bot L6

--- Bot/Aiogram/Pizza_shit/bot_telegram.py
+++ Bot/Aiogram/Pizza_shit/bot_telegram.py
@@ -6,9 +6,10 @@ async def on_startup(_):
     print('Бот вышел в онлайн')
 
 
-import handlers
+from handlers import client, admin, other
 
-handlers.client.register_handlers_client(dp)
-handlers.other.register_hadlers_other(dp)
+client.register_handlers_client(dp)
+admin.register_handlers_admin(dp)
+other.register_hadlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
